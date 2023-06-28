f70be7062634ef2916a5a5cfe704dc3f288e976b
Ryan Jackson <ryanjackson0y-@live.com>
2023-06-28 08:47:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ automl_settings = {
 }
 
 # TODO: add config object parameters 
-automl_config = AutoMLConfig()
+automl_config = AutoMLConfig(
+  task='forecasting',
+  primary_metric='normalized_root_mean_squared_error'
+
+  
+)
+# use with the energy days set
 
 # TODO: run an experiment using automl experiment class
